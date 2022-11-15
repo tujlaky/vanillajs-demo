de75b7d51f3c5be8f9c28b9d3f674b00d4cfdf0d
Tibor Ujlaky <tujlaky@gmail.com>
2022-11-15 23:56:40 +0100

    Basic forms

--- src/demo/demo.page.tsx
+++ src/demo/demo.page.tsx
@@ -12,7 +12,44 @@ export function DemoPage(
     counter: 99,
   });
 
+  function onSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const elements = Array.from(form.elements) as HTMLInputElement[]
+    const data = elements
+      .filter((element) => element.hasAttribute('name'))
+      .reduce(
+        (object, element) => ({
+          ...object,
+          [`${element.getAttribute('name')}`]: element.value,
+        }),
+        {} as { demo: string}
+      );
+
+    const number = parseInt(data.demo, 10);
+    
+    
+
+    console.log(data);
+
+    if (!isNaN(number)) {
+      console.log(number);
+      state$.next({
+        ...state$.value,
+        counter: number,
+      });
+    }
+  };
+
   return (
-    <TitleComponent>Test</TitleComponent>
+    <div>
+      <TitleComponent>Test</TitleComponent>
+
+      <form onSubmit={onSubmit}>
+        <input name="demo" type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
