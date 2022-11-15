a1df5ad634b887e4b8c5bbdaf3864a4a0cbfcc63
Tibor Ujlaky <tujlaky@gmail.com>
2022-11-15 17:26:45 +0100

    Add tailwind

--- src/title.component.ts
+++ src/title.component.ts
@@ -2,6 +2,7 @@ const TitleComponent = (): HTMLElement => {
   const h1 = document.createElement("h1");
 
   h1.innerText = "Hello world!";
+  h1.className = "text-3xl font-bold underline";
 
   return h1;
 };
